8a7ed7ae5b789950324340a9925248eed257e66b
pmb <pmb@heise.de>
2014-11-12 13:42:29 +0100

    init

---
+++ lib/Heise/Shariff/Service/Facebook.pm
@@ -0,0 +1,25 @@
+package Heise::Shariff::Service::Facebook;
+
+use Mojo::Base 'Heise::Shariff::Service';
+
+sub request {
+    my ($self, $url) = @_;
+
+    my $furl = Mojo::URL->new('https://api.facebook.com/method/fql.query');
+    $furl->query->param(format => 'json');
+    my $query = qq{select like_count from link_stat where url="$url"};
+    $furl->query->param(query => $query);
+
+    return [get => $furl];
+}
+
+sub extract_count {
+    my ($self, $res) = @_;
+    return $res->json->[0]->{like_count};
+}
+
+sub get_name {
+    return 'facebook';
+}
+
+1;
---
+++ log/README.md
@@ -0,0 +1,1 @@
+Contains log files (morbo: development.log; hypnotoad: production.log)
