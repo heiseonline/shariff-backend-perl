ec3cd4e1b185d80e6c5cecc31435086dcaf6d459
pmb <pmb@heise.de>
2014-12-02 11:19:02 +0100

    check response

--- lib/Heise/Shariff/Service/Facebook.pm
+++ lib/Heise/Shariff/Service/Facebook.pm
@@ -1,6 +1,7 @@
 package Heise::Shariff::Service::Facebook;
 
 use Mojo::Base 'Heise::Shariff::Service';
+use Mojo::Util qw(dumper);
 
 sub request {
     my ($self, $url) = @_;
@@ -15,7 +16,13 @@ sub request {
 
 sub extract_count {
     my ($self, $res) = @_;
-    return $res->json->[0]->{share_count};
+
+    if (ref $res->json eq 'ARRAY') {
+        return $res->json->[0]->{share_count};
+    } else {
+        warn dumper($res);
+        return 0;
+    }
 }
 
 sub get_name {
